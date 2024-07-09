95b6d03805b6f71265cc8f9e5163a9049edd607d
Ochala27 <ochalaonuche3@gmail.com>
2024-07-09 22:48:43 +0100

    add footer section with CSS styling
    - Implemented Footer section to provide navigation links and contact information.
    - Styled Footer with appropriate colors, spacing, and text size for a cohesive design.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import Header from './Header';
 import MainContent from './MainContent';
+import Footer from './Footer';
 
 const Layout = () => {
   return (
     <div>
       <Header />
       <MainContent />
+      <Footer/>
     </div>
   );
 };
